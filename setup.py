abf24d681d9397990b74902cfae9e260e5f74ec6
Travis Prosser <travisprosser@gmail.com>
2019-11-01 18:41:57 -0300

    Initial commit with structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="beer_sign_creator",
+    packages=find_packages(),
+    install_requires=[
+        "opencv-python",
+        "imageio"
+    ],
+    entry_points={
+        "console_scripts": [
+            'create-sign=beer_sign_creator.create_sign:main'
+        ]
+    }
+)
